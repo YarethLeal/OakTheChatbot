869a7d8d8b83bbc6734ebf1f00c8ed8a7dc8888b
YarethLeal <yarethleal65@gmail.com>
2024-08-12 12:55:33 -0600

    Correction api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = "https://oak-the-chatbot-api.vercel.app/"
 export const getHelloMessage = async () => {
     try {
         const response = await fetch(`${API_URL}/api/hello`);
